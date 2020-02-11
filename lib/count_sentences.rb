fe233182e6e154adaad146f5cb00e3694da5b762
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-11 23:28:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class String
 
-  def sentence?(string)
-    self
+  def sentence?
+    
   end
 
   def question?
@@ -17,21 +17,4 @@ class String
   def count_sentences
 
   end
-end
-class Dog
- 
-  attr_accessor :name, :owner
- 
-  def initialize(name)
-    @name = name
-  end
- 
-  def bark
-    "Woof!"
-  end
- 
-  def get_adopted(owner_name)
-    self.owner = owner_name
-  end
- 
 end
